bcdcd77fe87da687a4ce232c92b6104ce4cd1b16
iceBear67 <icebear67@sfclub.cc>
2024-12-27 12:23:21 +0800

    Improve BBTheme#isRight for various inputs

--- src/main/java/plugily/projects/buildbattle/handlers/themes/BBTheme.java
+++ src/main/java/plugily/projects/buildbattle/handlers/themes/BBTheme.java
@@ -23,40 +23,62 @@ package plugily.projects.buildbattle.handlers.themes;
 import java.util.List;
 
 public class BBTheme {
+    private static final char CHINESE_WORD_BEGIN = 0x4e00;
+    private static final char CHINESE_WORD_END = 0x9fa5;
+    private static final char ASCII_BEGIN_1 = 'A';
+    private static final char ASCII_END_1 = 'Z';
+    private static final char ASCII_BEGIN_2 = 'a';
+    private static final char ASCII_END_2 = 'z';
+    private static final char ASCII_BEGIN_3 = '0';
+    private static final char ASCII_END_3 = '9';
 
-  private final List<String> theme;
-  private final Difficulty difficulty;
+    private final List<String> theme;
+    private final Difficulty difficulty;
 
-  public BBTheme(List<String> theme, Difficulty difficulty) {
-    this.theme = theme.stream().map(it->it.toLowerCase().trim()).toList();
-    this.difficulty = difficulty;
-  }
-
-  public String getTheme() {
-    return theme.get(0);
-  }
-
-  public boolean isRight(String answer){
-      answer = answer.toLowerCase();
-      return theme.stream().anyMatch(answer::contains);
-  }
-
-  public Difficulty getDifficulty() {
-    return difficulty;
-  }
-
-  public enum Difficulty {
-    EASY(1), MEDIUM(2), HARD(3);
-
-    private final int pointsReward;
-
-    Difficulty(int pointsReward) {
-      this.pointsReward = pointsReward;
+    public BBTheme(List<String> theme, Difficulty difficulty) {
+        this.theme = theme.stream().map(BBTheme::stripWord).toList();
+        this.difficulty = difficulty;
     }
 
-    public int getPointsReward() {
-      return pointsReward;
+    public String getTheme() {
+        return theme.get(0);
     }
-  }
+
+    public boolean isRight(String answer) {
+        answer = stripWord(answer);
+        return theme.stream().anyMatch(answer::contains);
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    private static String stripWord(String word) {
+        var chars = word.toLowerCase().toCharArray();
+        var sb = new StringBuilder();
+        for (char a : chars) {
+            if (a <= CHINESE_WORD_END && a >= CHINESE_WORD_BEGIN
+                    || a >= ASCII_BEGIN_1 && a <= ASCII_END_1
+                    || a >= ASCII_BEGIN_2 && a <= ASCII_END_2
+                    || a >= ASCII_BEGIN_3 && a <= ASCII_END_3) {
+                sb.append(a);
+            }
+        }
+        return sb.toString();
+    }
+
+    public enum Difficulty {
+        EASY(1), MEDIUM(2), HARD(3);
+
+        private final int pointsReward;
+
+        Difficulty(int pointsReward) {
+            this.pointsReward = pointsReward;
+        }
+
+        public int getPointsReward() {
+            return pointsReward;
+        }
+    }
 
 }
